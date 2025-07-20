00992a0d3b48c7000c3db613aa3939039bf630aa
dezire <devarkamondal@gmail.com>
2025-07-20 11:30:39 +0530

    Initial push

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="bg-gray-100 rounded-md p-2 my-2">
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+
+</script>
+<button class="p-2 rounded-md bg-blue-700 text-white">Sign in</button>
+<h1 class="text-4xl text-center font-semibold my-2 mb-4">Dezire's YT Dashboard </h1>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/8AaGzbNTbWk?si=b6fcBo64o_Tmss2d" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen class="mx-auto"></iframe>
+
+<section>
+	<h2 class="text-3xl text-center my-4 font-semibold">Comments</h2>
+</section>
